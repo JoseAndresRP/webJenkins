5d0ba0e85aa1a9654eede899fe81c463925ba578
JoseAndresRP <andres.ruiz@forzalatam.com>
2021-09-25 11:24:29 -0600

    API - Node - Galletas

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',function(req,res) {
   res.status(200).send("Hello World");
 });
 
+app.get('/galletas',function(req,res) {
+  res.status(200).send("ten galletas");
+});
+
 module.exports = app.listen(process.env.PORT || 4000, ()=>{
     console.log("Alive!")
 });
