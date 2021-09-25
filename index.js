d586954fff4f739004cea6761e788d11cbb68fd3
JoseAndresRP <andres.ruiz@forzalatam.com>
2021-09-25 11:17:05 -0600

    API - Node - Fix configurations

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World");
+  res.status(200).send("Hello World");
 });
 
 module.exports = app.listen(process.env.PORT || 4000, ()=>{
